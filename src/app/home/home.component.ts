1d3f097dfcfc2187fbd7c33852402acfb646fc12
Ghilès BADJI <ghiles.badji@gmail.com>
2024-01-02 16:54:49 +0100

    feat: create card component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CardComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
